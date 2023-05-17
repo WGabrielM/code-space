dae1e39d1099e9c2ca5484ef9d4bba65566d3ede
Wellington <wellington.batista@lightbase.com.br>
2023-05-17 09:11:21 -0300

    finalizando desenvolvimento

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import PaginaInicial from "./paginas/PaginaInicial";
 import './styles/estilosGlobais.scss'
+import PaginaInicial from "./paginas/PaginaInicial";
 
 export default function App() {
-  return (
-   <PaginaInicial />
-  );
+  return <PaginaInicial/>;
 }
-
-
